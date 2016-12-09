0766fa16f925bf1682cf13700adc511dfb22a327
A-Kod <andrey_kodyk@mail.ru>
2016-12-09 14:51:59 +0300

    Update pop.cpp

--- tests/pop.cpp
+++ tests/pop.cpp
@@ -1,3 +1,19 @@
-/////////////
-//For stage 2
-/////////////
+#include "stack.hpp"
+#include "catch.hpp"
+
+SCENARIO("pop one element from stack with one elem")
+{
+    GIVEN("empty stack)
+    {
+        Stack<int> st;
+        st.push(0);
+        WHEN("pop")
+        {
+            st.pop();
+            THEN("st.count == 0")
+            {
+                REQUIRE(st.count() == 0);
+            }
+        }
+    }
+}
