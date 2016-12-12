fabd9161451c02f873ab3b9d0e100482a0475426
A-Kod <andrey_kodyk@mail.ru>
2016-12-12 12:33:17 +0300

    Rename pop.cpp to top.cpp

---
+++ tests/top.cpp
@@ -0,0 +1,19 @@
+#include "stack.hpp"
+#include "catch.hpp"
+
+
+SCENARIO("top one element from stack with one elem")
+{
+    GIVEN("stack with one elem")
+    {
+        Stack<int> st;
+        st.push(0);
+        WHEN("top")
+        {
+            THEN("st.top() == 0")
+            {
+              REQUIRE(st.top() == 0);
+            }
+        }
+    }
+}
