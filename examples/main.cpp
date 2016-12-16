65d8cb1bca8e43629b2263519d86b55df5ba36ec
A-Kod <andrey_kodyk@mail.ru>
2016-12-16 14:14:10 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,18 +2,55 @@
 
 #include "stack.hpp"
 
+class Element
+{
+   private:
+   int x;
+
+   public:
+   Element (int _x = 0)
+   {
+       x = _x;
+   }
+
+   int get_x()
+   {
+       return x;
+   }
+
+    void set_x(int _x)
+    {
+        x = _x;
+    }
+
+};
+
+
+
 using namespace std;
 
 
 int main()
 {
+    Element e[10];
+    Stack <Element> st_;
     Stack <int> st;
-    
+
+
+    for (int j = 0; j < 10 ; ++j)
+    {
+       e[j].set_x(j);
+        st_.push(e[j]);
+    }
+
+    std::cout << "Range of stack of Elements - " << st_.count() << std::endl;
+
+
     for (int i = 0; i < 10 ; ++i)
     {
         st.push(rand()%50 + 5);
     }
-    
+
     std::cout << "Range of stack - " << st.count() << std::endl;
     st.print();
 
