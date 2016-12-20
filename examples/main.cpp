20c3b2e168fb15f8e0b8fdcbc57715262e9cce63
A-Kod <andrey_kodyk@mail.ru>
2016-12-20 10:45:28 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -25,51 +25,27 @@ class Element
 
 };
 
-
-
-using namespace std;
-
-
 int main()
 {
     Element e[10];
     Stack <Element> st_;
     Stack <int> st;
-
-
     for (int j = 0; j < 10 ; ++j)
     {
        e[j].set_x(j);
         st_.push(e[j]);
     }
 
-    std::cout << "Range of stack of Elements - " << st_.count() << std::endl;
-
-
+    std::cout << "Range of stack of Elements - " << st_.count() << std::endl << st_.top().get_x() <<std::endl << std::endl;
     for (int i = 0; i < 10 ; ++i)
     {
         st.push(rand()%50 + 5);
+        std::cout << st.top() << std::endl;
     }
 
     std::cout << "Range of stack - " << st.count() << std::endl;
-    st.print();
-
     std:: cout << "Top of stack - " << st.top() << std::endl;
-    
     st.pop();
-    std::cout << "Stack after pop:" << std::endl;
-    st.print();
     std::cout << "Range of stack - " << st.count() << std::endl;
-    
-    try
-    {
-        for (int i = 0; i < 10; ++i)
-            st.pop();
-    }
-    
-    catch (stack_error& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
     return 0;
 }
